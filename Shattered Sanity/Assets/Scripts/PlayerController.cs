c5701b7f278fe8fd2fa4e4427ad01557f75492da
minigoat7 <163208729+minigoat7@users.noreply.github.com>
2024-05-07 11:26:58 +1200

    vb

---
+++ Shattered Sanity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.Space))   
+        {
+            rb.AddForce(Vector3.up * 500);
+        }
+    }
+}
